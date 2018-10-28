4f60c0fb6cbd37423cdb51cfda7e2e2f6ee96541
unnar123 <unnarj18@ru.is>
2018-10-28 22:06:20 +0000

    updateað stim

--- main.m
+++ main.m
@@ -7,4 +7,17 @@ clc
 mappa = pwd + "/Platform all subjects";
 stim = xlsread(mappa + "/Stimuli.xlsx");
 
+% Breytum 59 í 1 og 20 í 0.
+for i=1:length(stim)
+    
+    if  stim(i,2) == 59
+        
+        stim(i,2)=1;
+    end
+    if stim(i,2) == 20
+         stim(i,2)=0;
+    end
+end
+
+
 [len, averageTime] = averageTime(stim)
