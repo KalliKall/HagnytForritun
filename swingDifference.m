65010d3a3435f1147b1141d85c8fd9c2bfb69e86
stefaj9 <stefaj9@gmail.com>
2018-11-04 20:45:43 +0000

    Added comments to swingDifference.m

--- swingDifference.m
+++ swingDifference.m
@@ -1,17 +1,22 @@
 function swingDifference(data)
 
+    %Athugum hvort mappan sem myndinar eiga að fara í eru til
     if ~exist('myndir/swingDifference', 'dir')
+        %Búum til möppuna sem myndinar fara í 
         mkdir('myndir/swingDifference');
     end
     
+    %For loopa fyrir hvert data set, getum hoppað yfir 2 í einu
+    %vegna þess að oddatala er með open og jafntölur eru með closed
     for i=1:2:length(data)-1
         figure
 
+        %Setjum upp 1x2 graph, plottum gögnum frá data(i) sem er open
+        %úrslit og data(i+1) sem eru closed úrslit
         subplot(1,2,1);
         plot(data{i,1}(:,2),data{i,1}(:,3), ".");
         hold on;
-        title("Opin augu");            
-        %axis([-20 20 -20 40]);
+        title("Opin augu");     
         xlabel('Medial/lateral [Nm]'); 
         ylabel('Anterior/posterior [Nm]');
 
@@ -19,13 +24,14 @@ function swingDifference(data)
         plot(data{i+1,1}(:,2),data{i+1,1}(:,3), ".");
         hold on;
         title("Lokuð augu");
-        %axis([-20 20 -20 40]);
         xlabel('Medial/lateral [Nm]');
         ylabel('Anterior/posterior [Nm]');
 
+        %Búum til staðsetningu fyrir myndina og vistum þar
         path = '/myndir/swingDifference/' + string(data{i,2}(1)) + '.png';
         saveas(gcf, [pwd + path]);
         
+        %Lokum öllum myndum
         close all;
     end
 end
